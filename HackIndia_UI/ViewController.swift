3afa7396c349af5c527d8653b60587f9e56295bf
yashtamakuwala <neel64tamakuwala@gmail.com>
2015-07-15 23:35:26 +0530

    Initial Commit

---
+++ HackIndia_UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackIndia_UI
+//
+//  Created by YASH on 15/07/15.
+//  Copyright (c) 2015 YASH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
